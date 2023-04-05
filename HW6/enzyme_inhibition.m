71d727350f3c07deb5e52e742f4b1602fcf97f7f
Audrey Yang <audrey.butter0930@gmail.com>
2023-04-05 00:15:28 -0400

    Update enzyme_inhibition.m

--- HW6/enzyme_inhibition.m
+++ HW6/enzyme_inhibition.m
@@ -9,19 +9,36 @@ e0 = 0.1;
 vmax = k2*e0;
 km = (kn1 + k2)/k1;
 ki = kn3/k3;
+ks = kn1/k1;
 
 i = 0; % change as need be
-t = 0:0.1:10; % 0 <= s <= 10
+t = 0:0.1:10; % s = [0, 10]
 
-comp_inhib = @(t)((vmax*t)/(km*(1+(i/ki)+t)));
+% initialize output variables
+comp_inhib = zeros(3, length(t));
+allo_inhib = zeros(3, length(t));
 
-%{
- value checking
-for j= 1:10
-    x = (vmax*t)/(km*(1+(j/ki)+t))
-    
+for i = 0:2     % i = 0, 1, 2
+    for s = 1:length(t)
+        comp_inhib(i+1,s) = (vmax*t(s))/(km*(1+i/ki)+t(s));
+        allo_inhib(i+1,s) = vmax/(1+i/ki)*(t(s)/(ks+t(s)));
+    end
 end
-%}
 
-plot(t, comp_inhib(t))
-axis([0 10 0 0.0252])
+figure(1)
+plot(t, comp_inhib')
+legend('i=0','i=1','i=2')
+axis([0 10 0 vmax])
+xlabel('Substrate (s)')
+ylabel('V_c(s,i)')
+title('Competitive inhibition (w/QSS approx.)')
+shg
+
+figure(2)
+plot(t, allo_inhib')
+legend('i=0','i=1','i=2')
+axis([0 10 0 vmax])
+xlabel('Substrate (s)')
+ylabel('V_a(s,i)')
+title('Allosteric inhibition (w/eqm approx.)')
+shg
